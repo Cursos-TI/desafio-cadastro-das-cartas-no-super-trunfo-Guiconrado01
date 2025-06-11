bbcfb78f82e3fde4335236bd10f955d58db60ac4
Guilherme conrado <guiconrado2015@hotmail.com>
2025-06-11 15:40:35 +0000

    criei o printf

---
+++ Desafio.c
@@ -0,0 +1,45 @@
+ #include <stdio.h>
+
+int main(){
+    printf("Desafio Xadrez");
+    
+  char Codigo[10];
+  char Nome[15];
+  int Populacao;
+  float PIB;
+  int Pontos;
+
+
+  printf("Digite o Codigo: /n");
+  scanf("%f", &Codigo);
+
+  printf("Digite o Nome: /n");
+  scanf("%s", &Nome);
+
+  printf("Digite a Populacao: /n");
+  scanf("%d", &Populacao);
+
+  printf("Digite o PIB: /N");
+  scanf("%f", &PIB);
+
+  printf("Digite os Pontos: /n");
+  scanf("%d", &Pontos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }
